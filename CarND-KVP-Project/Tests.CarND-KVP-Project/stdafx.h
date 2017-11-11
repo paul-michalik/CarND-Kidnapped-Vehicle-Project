5f91dafd5293e442e9e55161245d4eeaeb9f89b6
Paul Michalik <paul.michalik@outlook.com>
2017-11-11 15:16:12 +0100

    Prepare Windows environment.

---
+++ CarND-KVP-Project/Tests.CarND-KVP-Project/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <math.h> 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <helper_functions.h>
+#include <map.h>
+#include <json.hpp>
+// TODO: reference additional headers your program requires here
---
+++ CarND-KVP-Project/Tests.CarND-KVP-Project/tests.kvp.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <particle_filter.cpp>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestsCarNDKVPProject
+{		
+	TEST_CLASS(TestClass_ParticleFilter)
+	{
+	public:
+        TEST_METHOD(init_should_create_expected_values)
+        {
+            Assert::Fail(L"Not implemented");
+        }
+	};
+}
